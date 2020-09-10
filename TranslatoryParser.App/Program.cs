268c14688be4a461102ed1de02eac0605ad7ea2e
Michał Droździk <m.drozdzik97@gmail.com>
2020-09-10 12:29:23 +0200

    Initial Commit

---
+++ TranslatoryParser.App/Program.cs
@@ -0,0 +1,49 @@
+﻿using Sprache;
+using System;
+using TranslatoryParser.Common;
+
+namespace TranslatoryParser.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (Prompt(out string expression))
+            {
+                Console.Clear();
+
+                try
+                {
+                    var parsed = Parser.ParseExpression(expression);
+                    Console.WriteLine("Parsed expression: {0}", parsed);
+                    Console.WriteLine("Result: {0}", parsed.Compile()());
+                }
+                catch (ParseException ex)
+                {
+                    Console.WriteLine("There was a problem with your input: {0}", ex.Message);
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        static bool Prompt(out string value)
+        {
+            Console.Write("Enter a numeric expression (1 + 15), or 'q' to quit: ");
+            var line = Console.ReadLine();
+
+            Console.WriteLine(Environment.NewLine);
+
+            if (line.ToLowerInvariant().Trim() == "q")
+            {
+                value = null;
+                return false;
+            }
+            else
+            {
+                value = line;
+                return true;
+            }
+        }
+    }
+}
